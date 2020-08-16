8b640f8d561513a1c3560e2044697abb3e78782c
Kailaash Jeevan <kailaash80@gmail.com>
2020-08-16 13:58:17 +0530

    Updates

---
+++ precache-manifest.b560f0498ec392566f484d9fb5eb1657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7515f517a6c060962b5e92da8a9776",
+    "url": "/filter/index.html"
+  },
+  {
+    "revision": "e109ecc5aae718e254b6",
+    "url": "/filter/static/css/main.0c05da6d.chunk.css"
+  },
+  {
+    "revision": "b47d7756d31efee88003",
+    "url": "/filter/static/js/2.ccda3140.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/filter/static/js/2.ccda3140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e109ecc5aae718e254b6",
+    "url": "/filter/static/js/main.bb88b15f.chunk.js"
+  },
+  {
+    "revision": "0180af4cf3067c203c2b",
+    "url": "/filter/static/js/runtime-main.8b8203f3.js"
+  }
+]);
